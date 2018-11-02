dc2f2d34272ba2d347307faf4459157a7c7bc1cb
David Skoda <dskoda1@binghamton.edu>
2018-11-02 11:40:13 -0400

    mui theme provider integrated

--- client/src/index.js
+++ client/src/index.js
@@ -3,16 +3,21 @@ import { render } from 'react-dom';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 import { Provider } from "react-redux";
+import { MuiThemeProvider } from '@material-ui/core/styles';
+
 import './index.css';
 
+import theme from './theme';
 import App from './containers/App';
 import { store } from './redux/store';
 
 render(
     <Provider store={store}>
-        <Router>
-            <App />
-        </Router>
+        <MuiThemeProvider theme={theme}>
+            <Router>
+                <App />
+            </Router>
+        </MuiThemeProvider>
     </Provider>,
     document.getElementById('root')
 );
--- client/src/theme.js
+++ client/src/theme.js
@@ -1,1 +1,40 @@
-import {createMuiTheme } from '@material-ui/core/styles';
+import { createMuiTheme } from '@material-ui/core/styles';
+
+// All the following keys are optional.
+// We try our best to provide a great default value.
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      light: '#5ddef4',
+      main: '#00acc1',
+      dark: '#007c91',
+      contrastText: '#000000',
+    },
+    secondary: {
+      light: '#484848',
+      main: '#212121',
+      dark: '#000000',
+      contrastText: '#ffffff',
+    },
+    // Used by `getContrastText()` to maximize the contrast between the background and
+    // the text.
+    contrastThreshold: 3,
+    // Used to shift a color's luminance by approximately
+    // two indexes within its tonal palette.
+    // E.g., shift from Red 500 to Red 300 or Red 700.
+    tonalOffset: 0.2,
+  },
+  typography: {
+    // Use the system font instead of the default Roboto font.
+    fontFamily: [
+      '"Helvetica Neue"',
+      'Roboto',
+      'Arial',
+      'sans-serif',
+    ].join(','),
+    // Could inject this value to enable toggling it
+    fontSize: 12
+  },
+});
+
+export default theme;
