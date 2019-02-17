78be641062ec6342b8f1023ffb6a88d7c2b836fc
David Skoda <dskoda1@binghamton.edu>
2019-02-17 12:18:21 -0500

    implement all user backend functionality in node

--- db/models/user.js
+++ db/models/user.js
@@ -2,7 +2,7 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
     username: DataTypes.STRING,
-    role: DataTypes.STRING,
+    password: DataTypes.STRING
   }, {});
   User.associate = function(models) {
     // associations can be defined here
--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express');
 const path = require('path');
-
+const bcrypt = require('bcrypt');
 const app = express();
-
+const cookieSession = require('cookie-session');
 const models = require('./db/models');
 
 // Serve static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 app.use(express.json());
+
+// Cookie based sessions
+app.use(cookieSession({
+  name: 'App-Session',
+  keys: ['my secret key!'],
+  maxAge: 24 * 60 * 60 * 1000 // 24 hours
+}))
 
 // Put all API endpoints under '/api'
 app.get('/api/passwords', (req, res) => {
@@ -21,6 +28,54 @@ app.post(`/api/users`, (req, res) => {
   }).then(user => res.json(user))
 })
 
+app.post('/api/register', (req, res) => {
+  // Verify username and password exist
+
+  // Hash password
+  bcrypt.hash(req.body.password, 10, (err, hash) => {
+    if (err) {
+      return res.status(424).json({err});
+    }
+    // Create new user
+    models.User.create({
+      username: req.body.username,
+      password: hash
+    }).then(user => {
+      req.session.username = user.username;
+      res.json({'username': req.body.username})
+    });
+  })
+});
+
+app.post('/api/login', (req, res) => {
+  models.User.findOne({ where: {username: req.body.username}}).then(user => {
+    if (!user) {
+      return res.status(401).json({'error': `Username ${req.body.username} not found`})
+    }
+    // compare the password with hash in db
+    bcrypt.compare(req.body.password, user.password, (err, match) => {
+      if (match) {
+        req.session.username = user.username;
+        return res.status(200).json({username: user.username});
+      }
+      res.status(401).end();
+    });
+  });
+});
+
+app.get('/api/profile', (req, res) => {
+  if (req.session.username) {
+    console.log(req.session.username);
+    return res.json({'username': req.session.username})
+  }
+  res.status(401).end();
+})
+
+app.post('/api/logout', (req, res) => {
+  req.session = null;
+  res.status(200).end();
+})
+
 // The "catchall" handler: for any request that doesn't
 // match one above, send back React's index.html file.
 app.get('*', (req, res) => {
@@ -29,7 +84,7 @@ app.get('*', (req, res) => {
 
 const port = process.env.PORT || 5000;
 
-models.sequelize.sync().then(function () {
+models.sequelize.sync({'alter': true}).then(function () {
   app.listen(port);
   console.log(`Listening on ${port}`)
 });
