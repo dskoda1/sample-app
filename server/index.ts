7b2f6be8223339b0f36b7a526bf851745450f19a
David Skoda <dskoda1@binghamton.edu>
2019-07-18 19:46:28 -0400

    test

--- server/index.ts
+++ server/index.ts
@@ -1,5 +1,5 @@
 console.log(`Node env: ${process.env.NODE_ENV}`)
-const app = require('./server/app').app;
+const app = require('./app').app;
 const port = process.env.PORT || 5000;
 
 app.listen(port);
