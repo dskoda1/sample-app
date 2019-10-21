80557cbd3d60123f2e85462904b8266099db9701
David Skoda <dskoda1@binghamton.edu>
2019-10-21 14:01:40 -0400

    fix apollo client uri for heroku deployment

--- client/src/apollo.tsx
+++ client/src/apollo.tsx
@@ -2,7 +2,7 @@ import ApolloClient from 'apollo-boost';
 import DefaultClient from 'apollo-boost';
 
 const client: DefaultClient<any> = new ApolloClient({
-  uri: 'http://localhost:3000/graphql',
+  uri: `/graphql`,
   credentials: 'include',
 });
 
