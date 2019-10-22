9d9cc67b0135b7f66f3b49334422e0eed6c1d687
David Skoda <dskoda1@binghamton.edu>
2019-10-22 15:31:19 -0400

    add lists page to the frontend

---
+++ client/src/apps/lists/index.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import { createStyles, Typography } from '@material-ui/core';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import makeStyles from '@material-ui/core/styles/makeStyles';
+// import Categories from './Categories';
+
+const useStyles = makeStyles(theme =>
+  createStyles({
+    root: {
+      flexGrow: 1,
+    },
+    header: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+    },
+    paper: {
+      padding: theme.spacing(2),
+      textAlign: 'center',
+    },
+  })
+);
+
+interface ListPageProps {}
+
+const ListPage: React.FunctionComponent<ListPageProps> = () => {
+  const classes = useStyles();
+  return (
+    <div className={classes.root}>
+      <Grid container justify={'center'} direction={'row'} spacing={3}>
+        <Grid item xs={12} className={classes.header}>
+          <Typography variant="h2">Your Lists</Typography>
+        </Grid>
+        <Grid item xs={12} md={3}>
+          <Paper className={classes.paper}>
+            <Typography variant="h5">hello</Typography>
+          </Paper>
+        </Grid>
+        {/*<Grid item xs={12} md={3}>*/}
+        {/*  <Paper className={classes.paper}>*/}
+        {/*    <Typography variant="h5">Purchases</Typography>*/}
+        {/*  </Paper>*/}
+        {/*</Grid>*/}
+        <Grid item xs={12} md={3}>
+          <Paper className={classes.paper}>
+            <Typography variant="h5">hello</Typography>
+          </Paper>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
+
+export default ListPage;
--- client/src/apps/workouts/Workout/LoggedOutView.js
+++ client/src/apps/workouts/Workout/LoggedOutView.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const LoggedOutView = () => (
   <div>
-    Please <Link to="/auth">login</Link> to see your workouts!
+    Please <Link to="/auth">login</Link> to view your data!
   </div>
 );
 
