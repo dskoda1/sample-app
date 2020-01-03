9357382b8408cf096efed9941a2ee85ea4ff54e9
David Skoda <dskoda1@binghamton.edu>
2020-01-03 16:26:28 -0500

    remove LinkItem abstraction

--- client/src/components/navbar/AppDrawer.js
+++ client/src/components/navbar/AppDrawer.js
@@ -1,12 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import List from '@material-ui/core/List';
 import Divider from '@material-ui/core/Divider';
 import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+
+import ListItem from '@material-ui/core/ListItem';
+
+import ListItemIcon from '@material-ui/core/ListItemIcon';
+import ListItemText from '@material-ui/core/ListItemText';
 import DirectionsRunIcon from '@material-ui/icons/DirectionsRun';
 import AttachMoneyIcon from '@material-ui/icons/AttachMoney';
 import ListIcon from '@material-ui/icons/List';
-import LinkItem from 'components/navbar/LinkItem';
 
 const AppDrawer = ({ classes: { toolbar }, onClose, onOpen, isOpen }) => (
   <SwipeableDrawer open={isOpen} onClose={onClose} onOpen={onOpen}>
@@ -14,16 +19,38 @@ const AppDrawer = ({ classes: { toolbar }, onClose, onOpen, isOpen }) => (
       <div className={toolbar} />
       <Divider />
       <List>
-        <LinkItem linkTo={'/workouts'} text={'Workouts'}>
-          <DirectionsRunIcon />
-        </LinkItem>
-        <LinkItem linkTo={'/finance'} text={'Finance'}>
-          <AttachMoneyIcon />
-        </LinkItem>
-        <LinkItem linkTo={'/activity'} text={'Activity'} />
-        <LinkItem linkTo={'/lists'} text={'Lists'}>
-          <ListIcon />
-        </LinkItem>
+        <Link to="/workouts" style={{ textDecoration: 'none', color: '#000' }}>
+          <ListItem button>
+            <ListItemIcon>
+              <DirectionsRunIcon />
+            </ListItemIcon>
+            <ListItemText primary="Workouts" />
+          </ListItem>
+        </Link>
+        <Link to="/finance" style={{ textDecoration: 'none', color: '#000' }}>
+          <ListItem button>
+            <ListItemIcon>
+              <AttachMoneyIcon />
+            </ListItemIcon>
+            <ListItemText primary="Finance" />
+          </ListItem>
+        </Link>
+        <Link to="/activity" style={{ textDecoration: 'none', color: '#000' }}>
+          <ListItem button>
+            <ListItemIcon>
+              <ListIcon />
+            </ListItemIcon>
+            <ListItemText primary="Activity" />
+          </ListItem>
+        </Link>
+        <Link to="/lists" style={{ textDecoration: 'none', color: '#000' }}>
+          <ListItem button>
+            <ListItemIcon>
+              <ListIcon />
+            </ListItemIcon>
+            <ListItemText primary="Lists" />
+          </ListItem>
+        </Link>
       </List>
       <Divider />
     </div>
